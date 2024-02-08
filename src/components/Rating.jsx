f5ec197746f2274ce3b4499801f9c8bf1f694a68
Liaosha <alyosasha@gmail.com>
2024-02-08 18:09:57 +0100

    Remplacement des fichiers

---
+++ src/components/Rating.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ratingStar from "../images/rating-star.svg";
+import greyStar from "../images/rating-greystar.svg";
+import '../styles/Rating.scss';
+
+function Rating({ rating }) {
+    const stars = [];
+    for (let i = 0; i < rating; i++) {
+        stars.push(<img className='rating__img' src={ratingStar} alt="Rating star" key={i} />);
+    }
+    for (let i = rating; i < 5; i++) {
+        stars.push(<img className='rating__img' src={greyStar} alt="Grey star" key={i} />);
+    }
+    return <div className='rating'>{stars}</div>;
+}
+
+export default Rating;
